34531079d69d79ec64874f21ff63c4d2d2047661
Pedro Esposito <pedro.esposito99@gmail.com>
2022-09-15 12:04:13 -0300

    Terminando detalles carrito

--- src/Componentes/Cart/CartList.jsx
+++ src/Componentes/Cart/CartList.jsx
@@ -3,11 +3,24 @@ import { useContext } from "react"
 import { CartContext } from "../Context/CartContext"
 export default function CartList(){
   const itemsCart = useContext(CartContext)
+  let total = 0
+
+  if(itemsCart.cart.length===0){
+    return(
+      <>
+        <h1>Tu carrito esta vacio</h1>
+      </>
+    )
+  }
   return(
   <>
     <button className="btn btn-danger col-7" onClick={()=>itemsCart.clear()}>Vaciar carrito</button>
     {itemsCart.cart.map((product,index)=>{
-    return <Cart productsMaped={product} key={index} />})}
+      total += product.price * product.cant
+      return <Cart productsMaped={product} key={index} />
+    }
+    )}
+    <div className="fs-4 text-center col-12 text-light">Tu total es ${total}</div>
   </>
   )
 }
--- src/Componentes/NavAndFooter/CartWidget.jsx
+++ src/Componentes/NavAndFooter/CartWidget.jsx
@@ -3,11 +3,17 @@ import { useContext } from "react"
 import { Link } from "react-router-dom"
 export default function CartWidget(){
   const {cart} = useContext(CartContext)
+  let numberOfItems = 0
+  cart.map(item=>numberOfItems += item.cant)
+  if(numberOfItems===0){
+    return
+  }
   return(
     <>
+    
     <Link to="/carrito"><button className= "d-flex btn btn-light me-2 mt-2 position-absolute top-0 end-0">
       <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAA9ElEQVRIid3UPy5EURzF8Q8FCgURNY0FSMQaDInKVqxBZgMqyRTCCqaU0NuBckaFkjwKf4r7S7yMZNz7vCfhJLf7/s45N/eXy3/RG97jDLoIeK0FPGO1ixAY1oKansu64exEwHELJafefgY30WS3wHQJ9zG3/x18GOCwIKAfM1c58DKepM3ayODXUQW/ldtoEI36Gex5sKe55rAZQw9YmMJtS80rrJUEwLX81TwqNV/BXYZxhTPMlwachMGFtLqtalH6Ll7kbVGx5vCo2TeRrQN5b9A4oHPt4RZj9Frgvmjs89qjn3CT3/WvqSe1G2GnBe4P6wMWynZC39nrkQAAAABJRU5ErkJggg==" alt="carrito" width="30px" height="30px" className="align-self-center ms-auto"/>
-      <small className="fs-5">{cart.length}</small>
+      <small className="fs-5">{numberOfItems}</small>
     </button></Link>
     </>
   )
